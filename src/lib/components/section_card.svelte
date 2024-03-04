d2d013fcab6fda1f45012b412b5ac0879d49dc85
LMGerard <52181828+LMGerard@users.noreply.github.com>
2024-03-04 14:10:36 +0100

    Update section_card.svelte

--- src/lib/components/section_card.svelte
+++ src/lib/components/section_card.svelte
@@ -2,9 +2,9 @@
   export let title:string;
 </script>
 
-<div class="bg-red-950 w-full h-fit bg-opacity-90 p-2 rounded-md">
-  <div class="flex flex-col items-center gap-5 text-white">
-    <p class="font-zagoth text-3xl">{title}</p>
+<div class="bg-red-900 w-full h-fit bg-opacity-100 p-2 rounded-md relative">
+  <div class="flex flex-col items-center gap-5">
+    <p class="font-zagoth text-3xl text-white">{title}</p>
     <slot/>
   </div>
 </div>

cd0c03785f25746c4e6513b60f61a07251ada032
LMGerard <52181828+LMGerard@users.noreply.github.com>
2024-03-04 14:10:24 +0100

    Create SearchItemsTable.svelte

---
+++ src/lib/components/miscellaneous/SearchItemsTable.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let title:string;
+  export let headers:string[];
+</script>
+
+
+<table class="bg-white">
+  <caption class="bg-gray-300 font-zagoth text-3xl tracking-widest">
+    {title}
+  </caption>
+  <thead class="text-sm">
+    <tr>
+      {#each headers as header}  <th>  {header}  </th>  {/each}
+    </tr>
+  </thead>
+  <tbody class="text-xs text-center child:border-t-1 child:border-black child:child:pt-2 child:child:pb-2">
+    <slot/>
+  </tbody>
+</table>

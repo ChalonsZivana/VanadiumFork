92815d3066898766d044498a57c2974460d77947
LMGerard <52181828+LMGerard@users.noreply.github.com>
2024-12-06 14:21:29 +0100

    only k've

--- src/routes/(unauthorized)/fignos-[numero_table]/+page.server.ts
+++ src/routes/(unauthorized)/fignos-[numero_table]/+page.server.ts
@@ -13,8 +13,9 @@ export async function load({params}){
   if(isNaN(numero_table)) throw error(404);
 
 
-  let categories = await prisma.categories.findMany({where:{id_boquette:223}, orderBy:{nom:'asc'}});
+  let categories = await prisma.categories.findMany({where:{id_boquette:223, id_categorie:132}, orderBy:{nom:'asc'}});
   let produits = await prisma.produits.findMany({where:{id_boquette:223}, orderBy:{nom:'asc'}});
+
   return { 
     produits,categories, numero_table
   };
